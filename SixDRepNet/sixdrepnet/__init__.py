4b16efdede5aea524080567ec385418fc8799486
Thomas Tam <yuchowt@andrew.cmu.edu>
2022-12-13 03:00:59 -0500

    Add files via upload

---
+++ SixDRepNet/sixdrepnet/__init__.py
@@ -0,0 +1,12 @@
+# from SixDRepNet.sixdrepnet.regressor import SixDRepNet_Detector as SixDRepNet
+# from sixdrepnet.regressor import SixDRepNet_Detector as SixDRepNet
+
+
+"""
+6DRepNet.
+
+Accurate and unconstrained head pose estimation.
+"""
+
+__version__ = "0.1.2"
+__author__ = 'Thorsten Hempel'
