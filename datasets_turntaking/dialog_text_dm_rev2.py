3cc68d7e9086cbecc38938db724e18c73ec86657
Thomas Tam <yuchowt@andrew.cmu.edu>
2022-12-18 14:36:26 -0500

    Update dialog_text_dm_rev2.py

--- datasets_turntaking/dialog_text_dm_rev2.py
+++ datasets_turntaking/dialog_text_dm_rev2.py
@@ -819,7 +819,9 @@ class ConversationalDM2(pl.LightningDataModule):
         # create a tensor to store the result
         input_speaker_pad = torch.zeros_like(input_word_pad)
         for i in range(len(input_speaker)):
+          print(i)
           input_speaker_element = torch.nn.functional.pad(input_speaker[i], (0, self.max_length-len(input_speaker[i])), 'constant', input_speaker[i][-1].item())
+          print(input_speaker_element)
           input_speaker_pad[i] = input_speaker_element
 
         # create a tensor to store the result
